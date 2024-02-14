6b5ec398638b82d1517effb46ee6c001abb83618
FU-QAQ <fuquanyuo@gmail.com>
2024-02-14 21:32:05 +0800

    del src dir

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "page/base-arch/arch-page/ArchPage.h"
-
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    ArchPage ap;
-    ap.show();
-    return QApplication::exec();
-}
