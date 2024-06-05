e71e39d7eb2a8020f65dcd867b924efb72498ab9
FUQUANYUO <74005988+FUQUANYUO@users.noreply.github.com>
2024-06-05 18:09:13 +0800

    rm conflict msg
    删除冲突提示字符段

--- src/utils/encrypt/EncryptString.cpp
+++ src/utils/encrypt/EncryptString.cpp
@@ -11,18 +11,7 @@
 void handleError(){
     LOG("ERROR : " << ERR_error_string(ERR_get_error(), nullptr))
 }
-
 EncryptString *EncryptString::obj = nullptr;
-<<<<<<< HEAD
-std::string toHex(const unsigned char *buf, size_t len) {
-    std::ostringstream oss;
-    oss << std::hex << std::setfill('0');
-    for (size_t i = 0; i < len; ++i) {
-        oss << std::setw(2) << static_cast<int>(buf[i]);
-    }
-    return oss.str();
-}
-
 std::string EncryptString::encryptString(const std::string& str) {
     // md5
     unsigned char encryptArr[MD5_DIGEST_LENGTH];
@@ -34,20 +23,6 @@ std::string EncryptString::encryptString(const std::string& str) {
     return ss.str();
 }
 
-=======
-
-std::string EncryptString::encryptString(const std::string& str) {
-    // md5
-    unsigned char encryptArr[MD5_DIGEST_LENGTH];
-    MD5(reinterpret_cast<const unsigned char*>(str.c_str()), str.length(), encryptArr);
-    std::stringstream ss;
-    for(unsigned char i : encryptArr){
-        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(i);
-    }
-    return ss.str();
-}
-
->>>>>>> remotes/origin/V0.0.1a-dev-bug
 EncryptString *EncryptString::getEncryptObj() {
     if(obj == nullptr){
         obj = new EncryptString;
