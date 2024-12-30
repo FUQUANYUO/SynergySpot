c3fa1ad71965d0b64f82243a1835d5246e9895d7
FU-QAQ <fuquanyuo@gmail.com>
2024-12-31 02:22:49 +0800

    fixed : fixed bug due to linux platform (centos and ubuntu) cause docker-compose compiler occur link error

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # 停止脚本执行，如果 make 或 ./../out/Server 命令失败
 set -e
-
 
 if [ ! -d "./server/cmake-build" ]; then
     mkdir ./server/cmake-build && cd ./server/cmake-build
@@ -9,29 +8,35 @@ else
     cd ./server/cmake-build
 fi
 
-cmake ..
+cmake ../..
 
 # 解压LinuxLib
 cd ../../src/lib-common/lib/Linux
-if [ $(ls -A | grep -cE "^LinuxLib.tar.gz") -eq 1 ]; then
-  echo "lack of linux lib , then uncompress from LinuxLib.tar.gz"
-  tar -xzvf LinuxLib.tar.gz
+
+# 初始化解压，若已经初始化则不解压及init mysql
+if [ $(ls -1 | grep -E "LinuxLib-ubuntu\.tar\.gz|LinuxLib-centos\.tar\.gz" | wc -l) -eq 2 ] && [ $(ls -1 | grep -vE "LinuxLib-ubuntu\.tar\.gz|LinuxLib-centos\.tar\.gz" | wc -l) -eq 0 ]; then
+  echo "lack of LinuxLib, uncompressing from LinuxLib-ubuntu.tar.gz"
+  tar -xzvf LinuxLib-ubuntu.tar.gz || { echo "Failed to uncompress LinuxLib-ubuntu.tar.gz"; exit 1; }
+
+# init mysql
+  service mysql start
+  service mysql status
+  echo "------------------ begin to do init mysql ------------------"
+  mysql -uroot -pSS123456 < ../../doc/数据库/sys_init.sql
+  mysql -uroot -pSS123456 < ../../doc/数据库/synergy_spot.sql
+  mysql -uroot -pSS123456 < ../../doc/数据库/synergy_spot_init.sql
+  echo "------------------ init done ------------------"
 else
-  echo "skip get linux lib..."
+  echo "The current directory does not contain exactly two files, skipping uncompress and init mysql"
 fi
-tar -xzvf LinuxLib.tar.gz
 
 cd ../../../../server/cmake-build
 
 # 执行 make 命令
-make 
+echo "------------------ begin to do make ------------------"
+make
+echo "------------------ make done ------------------"
 
-service mysql start
-
-service mysql status
-mysql -uroot -pSS123456 < ../../doc/数据库/sys_init.sql
-mysql -uroot -pSS123456 < ../../doc/数据库/synergy_spot.sql
-mysql -uroot -pSS123456 < ../../doc/数据库/synergy_spot_init.sql
 
 # 执行编译后的程序
 ./../out/Server
